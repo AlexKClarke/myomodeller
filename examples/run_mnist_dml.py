3a7710269556d5e603ff2f4879c68314569b8fa3
AlexKClarke <61230358+AlexKClarke@users.noreply.github.com>
2023-05-12 11:35:24 +0100

    Adds DML

---
+++ examples/run_mnist_dml.py
@@ -0,0 +1,48 @@
+"""Example of training a 2D convolutional neural network to learn a deep 
+metric embedding on 8x8 MNIST images"""
+
+import os, sys
+
+if os.path.basename(os.getcwd()) != "pyrepo":
+    os.chdir("..")
+sys.path.append(os.path.abspath(""))
+
+from training import TrainingModule
+
+if __name__ == "__main__":
+    # The entirety of the run is defined by config, which will be unpacked
+    # at run time. The training module will look in the loader_modules
+    # and update_modules __init__.py for the named modules and then pass
+    training_module_config = {
+        "log_name": "mnist_dml",
+        "update_module_config": {
+            "update_module_name": "DeepMetricLearner",
+            "update_module_kwargs": {
+                "num_classes": 10,
+                "margin": 0.1,
+                "optimizer": "AdamW",
+                "optimizer_kwargs": {"lr": 0.001},
+            },
+            "maximize_val_target": True,
+            "network_config": {
+                "network_name": "blocks.Conv2dBlock",
+                "network_kwargs": {
+                    "input_shape": [1, 8, 8],
+                    "output_shape": [128],
+                    "out_chans_per_layer": [32, 64],
+                    "output_activation": None,
+                },
+            },
+        },
+        "loader_module_config": {
+            "loader_module_name": "MNIST",
+            "loader_module_kwargs": {"batch_size": 256},
+        },
+    }
+
+    # Once the config is defined it can be passed to an instance of the
+    # training module
+    training_module = TrainingModule(training_module_config)
+
+    # Train the model and pass the results to tensorboard
+    training_module.train()
