03b1ad6312635257fbed7e68d284c346e4889325
Teo56 <matteo.pizzi19@imperial.ac.uk>
2024-02-13 17:04:23 +0000

    - first commit

--- loader_modules/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-All loader module subclasses need to be placed in this init to be seen by the 
-training module. See training.core for the parent class.
-"""
-
-from loader_modules.mnist import MNIST
-from loader_modules.mnist28 import MNIST28
-from loader_modules.mu_labelled_emg import MotorUnitLabelledEMG
-from loader_modules.raw_emg import RawEMGLabelled
--- visualization_modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from visualization_modules.latents_visualization import VisualizeLatentSpace
-from visualization_modules.reconstructed_sample_visualization import VisualizeReconstructedSamples
-
