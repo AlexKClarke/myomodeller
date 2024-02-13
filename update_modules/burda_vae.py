4084c673064b6fb957956b3f09ae404526686183
Teo56 <matteo.pizzi19@imperial.ac.uk>
2024-02-13 15:40:26 +0000

    - implemented importance_weighted_autoencoder (burda likelihood)

--- update_modules/burda_vae.py
+++ update_modules/burda_vae.py
@@ -5,7 +5,7 @@ import torch.distributions as td
 from training import UpdateModule
 
 
-class VariationalAutoencoder(UpdateModule):
+class BurdaVariationalAutoencoder(UpdateModule):
     """A module that trains a variational autoencoder using a beta ELBO"""
 
     def __init__(
@@ -19,6 +19,7 @@ class VariationalAutoencoder(UpdateModule):
         early_stopping_kwargs: Optional[Dict] = None,
         beta_step: float = 1e-2,
         max_beta: float = 1.0,
+        burda_samples: int = 5,
     ):
         super().__init__(
             network,
@@ -33,27 +34,35 @@ class VariationalAutoencoder(UpdateModule):
         self.beta = 0.0
         self.beta_step = beta_step
         self.max_beta = max_beta
+        self.burda_samples = burda_samples
 
-    def _calculate_elbo_terms(self, x: torch.Tensor):
+
+    def _calculate_burda_likelihood(self, x: torch.Tensor, burda_samples: int):
         # Get the posterior and reconstuction parameters
         z_mean, z_var = self.network.encode(x)
-        z = self.network.sample_posterior(z_mean, z_var)
-        recon_mean, recon_var = self.network.decode(z)
+        z = self.network.sample_posterior(z_mean, z_var, burda_samples)
+        recon_mean, recon_var = self.network.decode(z) # returns (batch_size, burda_samples, 1, heigth, width) for both mean and variance
+
+        # Expand matrices for the burda reps
+        z_mean = z_mean.unsqueeze(1).repeat(1, burda_samples, 1)
+        z_var = z_var.unsqueeze(1).repeat(1, burda_samples, 1)
+        x = x.repeat(1, burda_samples, 1, 1)
 
         # Create distributions
         posterior_dist = td.MultivariateNormal(z_mean, z_var.diag_embed())
-        prior_dist = td.MultivariateNormal(
-            torch.zeros_like(z_mean), torch.ones_like(z_var).diag_embed()
-        )
-        recon_dist = td.Normal(recon_mean, recon_var)
+        prior_dist = td.MultivariateNormal(torch.zeros_like(z_mean), torch.ones_like(z_var).diag_embed())
+        recon_dist = td.Normal(recon_mean.squeeze(), recon_var.squeeze())
 
-        # Get the KL divergence between posterior and prior
-        kld_loss = td.kl_divergence(posterior_dist, prior_dist).mean()
+        # Calculate log probabilities
+        p_hi = prior_dist.log_prob(z)
+        p_xGhi = recon_dist.log_prob(x).sum((-1, -2))  # Sum along the image dimensions
+        q_hiGx = posterior_dist.log_prob(z)
 
-        # Get the log probability of x given the reconstruction distribution
-        recon_loss = recon_dist.log_prob(x).sum(1).mean()
+        # Calculate the ratio
+        ratio = torch.logsumexp(p_xGhi + p_hi - q_hiGx, dim=-1) + torch.log(torch.tensor(1/burda_samples)) # addition to compute the mean (log of product rule)
+        loss = ratio.mean()
 
-        return -recon_loss, kld_loss
+        return loss
 
     def _calculate_recon_r2(self, x: torch.Tensor):
         x_recon = self.network.forward(x)
@@ -66,18 +75,16 @@ class VariationalAutoencoder(UpdateModule):
         return r2.mean()
 
     def training_step(self, batch, batch_idx):
-        recon_loss, kld_loss = self._calculate_elbo_terms(batch[0])
-        elbo = recon_loss + self.beta * kld_loss
+        #recon_loss, kld_loss = self._calculate_elbo_terms(batch[0])
+        loss = self._calculate_burda_likelihood(batch[0], burda_samples=self.burda_samples)
 
         self.training_step_outputs.append(
             {
-                "training_elbo": elbo,
-                "training_recon": recon_loss,
-                "training_kld": kld_loss,
+                "training_loss": loss,
                 "beta": torch.tensor(self.beta),
             }
         )
-        return elbo
+        return loss
 
     def on_train_epoch_end(self):
         if self.beta < self.max_beta:
