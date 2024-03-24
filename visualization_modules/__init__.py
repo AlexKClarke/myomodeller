ba730458b89a55b22aa54817681b0979e13b757f
Teo56 <matteo.pizzi19@imperial.ac.uk>
2024-03-24 19:35:34 +0100

    - visualization branch update

--- visualization_modules/__init__.py
+++ visualization_modules/__init__.py
@@ -1,4 +1,4 @@
 from visualization_modules.latents_visualization import VisualizeLatentSpace
 from visualization_modules.reconstructed_sample_visualization import VisualizeReconstructedSamples
-#from visualization_modules.conv_filters_visualization import VisualizeConvFilters
+from visualization_modules.convolutional_filters_visualization import VisualizeConvolutionalFilters
 
